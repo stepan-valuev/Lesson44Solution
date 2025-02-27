4bab6e441c294dcbf984998400f932b91506dc73
Stepan Valuev <litovets008@gmail.com>
2025-02-27 17:25:41 +0300

    Add project files.

---
+++ Task01/main.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//
+//int sum_even_elements(int* vector, int size) {
+//	int sum = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (vector[i] % 2 == 0) {
+//			sum += vector[i];
+//		}
+//	}
+//	return sum;
+//}
+//
+//int main() {
+//	int vector[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//
+//	cout << "Sum of even elements is "
+//		<< sum_even_elements(vector, 10) << "\n";
+//	return 0;
+//}
